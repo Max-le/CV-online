5fd5a0c2ccfd310c0b86f87de2b8376c7e384117
Unknown <max.09@outlook.com>
2018-01-07 16:40:35 +0100

    Masquer button est mtn inutle

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Html code for email
 var emailhtml = "<a id='email' hidden href='mailto:max.09@outlook.com'> max.09@outlook.com</a>"
 	
-	
+
 	//  Jquery stuff 
 
 	//.ready method is triggered when page is loaded.
 	$(document).ready(function(){
 		$("button").click(function() {
+			
 			//Remplacer button par emailhtml
 			$("button").replaceWith(emailhtml);
 			$("#email").slideDown();
-			$("button").hide();
 
 
 		});

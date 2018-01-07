f5bbe070b0a16229ee81e3d1c3a861fc38fc3f6a
Unknown <max.09@outlook.com>
2018-01-07 16:27:18 +0100

    Placing javascript code in external file ( script.js )

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-var TodayDate = new Date();
-var eutype = TodayDate.toLocaleDateString();
-var ustype = TodayDate.toDateString();
 
-document.write(eutype + " = EU format" +"<br>");
-document.write(ustype + " = US format" + "<br>");
+	<!--  Jquery stuff -->	
+	<script>
+	//.ready method is triggered when page is loaded.
+	$(document).ready(function(){
+		$("button").click(function() {
+			$("#email").slideDown();
+			$("button").hide();
 
-function ChangeColor () {
-	var section = document.getElementsByClassName("section")
-	section[0].style.color = 'red';
-	section[1].style.color = 'red';
-	section[2].style.color = 'red';
-	n = section.length
-	console.log("Nombres de section(s) : " + n);
-	 
-	
 
-}
+		});
+	});
 
+</script>

11e17132f9d5af5466d2c21882a536432f89b5da
Max-le <max.09@outlook.com>
2018-01-07 17:43:50 +0100

    Slidedown() seems to work only if email element is hidden

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Html code for email
-var emailhtml = "<a id='email' href='mailto:max.09@outlook.com'> max.09@outlook.com</a>"
+var emailhtml = "<a id='email' hidden='true' href='mailto:max.09@outlook.com'> max.09@outlook.com</a>"
 
 
 	//  Jquery stuff 

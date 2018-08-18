aad567d3f092ecc4e0c1d5499fff5140a3d88a28
Max-le <max.09@outlook.com>
2018-08-18 12:02:48 +0200

    Delete unnecessary code

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// Html code for email
-var emailhtml = "<a id='email' hidden='true' href='mailto:max.09@outlook.com'> max.09@outlook.com</a>"
 
-
-jQuery( function() {
-	jQuery( "a" ).tooltip({show: {delay:150 }});
-
-
-} );
 jQuery(document).ready(function(){
 
 	console.log("document ready !");
 
-	jQuery("button-mail").click(function(){
-
-        //Remplacer button par emailhtml
-        jQuery("button").replaceWith(emailhtml);
-        jQuery("#email").slideDown();
-
-
-    });
 	jQuery("#TOEFL").click(function(){
 		jQuery("#toefl_scores").slideToggle();
 	});

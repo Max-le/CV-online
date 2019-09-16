adf6837ba21851543cf155f48062088721d788cb
Max <max@Maxs-MacBook-Air.local>
2019-09-16 14:44:37 +0200

    reindent

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 var count_clicks = 0 ;
 
 jQuery(document).ready(function(){
-
+	
 	console.log("document ready !");
-
+	
 	jQuery("#TOEFL").click(function(){
 		jQuery("#toefl_scores").slideToggle();
 	});
-		jQuery("#name").click(function(){
-			var newcolor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-			console.log('New color value :')
-			console.log(newcolor)
-
-			count_clicks = count_clicks + 1
-			console.log("clicks on name : ", count_clicks);
-			if (count_clicks == 15) {
-				console.log('EASTER EGG !!!')
-				/*Launch game*/
-				window.open("snake.html","_self")
-
-
-			}
-
-			$(this).css('color', newcolor);
-
-
+	jQuery("#name").click(function(){
+		var newcolor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+		console.log('New color value :')
+		console.log(newcolor)
+		
+		count_clicks = count_clicks + 1
+		console.log("clicks on name : ", count_clicks);
+		if (count_clicks == 15) {
+			console.log('EASTER EGG !!!')
+			/*Launch game*/
+			window.open("snake.html","_self")
+			
+			
+		}
+		
+		$(this).css('color', newcolor);
+		
+		
 	});
-
-
+	
+	
 });

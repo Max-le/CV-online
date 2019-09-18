4542723be6373d32cc92ce6f73b937fb25f8234c
Max <max@Maxs-MacBook-Air.local>
2019-09-18 11:25:19 +0200

    change easter egg

--- script.js
+++ script.js
@@ -14,14 +14,24 @@ jQuery(document).ready(function(){
 		var newcolor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 		console.log('New color value :')
 		console.log(newcolor)
-		
 		count_clicks = count_clicks + 1
 		console.log("clicks on name : ", count_clicks);
-		if (count_clicks == 15) {
-			console.log('EASTER EGG !!!')
-			/*Launch game*/
-			window.open("snake.html","_self")
+		if (count_clicks % 9 == 0) {
+			console.log('EASTER EGG !!!');
+			$('#name').effect('pulsate');
+			$('#iconCard').effect('shake', {direction:"up"});
+			$('#iconCard').animate({color:'black'},800, function name(params) {
+				$('#iconGit').effect('shake', {direction:"up"});
+
+				$('#iconGit').animate({color:'yellow'},800, function name(params) {
+					$('#iconIn').effect('shake', {direction:"up"});
+					$('#iconIn').animate({color:'red'},800)
+				})
+			})	
 			
+		
+						
+				
 			
 		}
 		

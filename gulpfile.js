69120d4b0c0b9387513baca5040587f663da6b89
Max <max.09@outlook.xom>
2020-03-10 22:47:27 +0100

    edit default behavior so it doesn't do "jekyll build"

--- gulpfile.js
+++ gulpfile.js
@@ -88,4 +88,4 @@ gulp.task('watch', function() {
   gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], gulp.series(['jekyll-rebuild']));
 });
 
-gulp.task('default', gulp.series(['js', 'sass', 'fonts', 'browser-sync', 'watch']));
+gulp.task('default', gulp.series(['js', 'sass', 'fonts', 'imagemin']));
